b0dd59e45e6d8109999bf6cc9efd5b75acd4ef74
Hugo Ramirez <cucobein@gmail.com>
2024-07-01 18:24:02 -0600

    Initial Commit

---
+++ pexels-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pexels-test
+//
+//  Created by Hugo Ramirez on 01/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pexels-test/pexels_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  pexels_testApp.swift
+//  pexels-test
+//
+//  Created by Hugo Ramirez on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct pexels_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
